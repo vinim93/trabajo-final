76b9d1f31641fd9e10e9f1e1ed61dc3a16685ae6
Manuel <vinim9395@gmail.com>
2020-10-10 01:10:46 -0500

    Updates

---
+++ precache-manifest.9c2d6ffb104050d1cd1fa0ec725806b6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc15251ee3292af62e892e6279a849f0",
+    "url": "/vinim93/trabajo-final.git/index.html"
+  },
+  {
+    "revision": "a33a30e03a728da90b3a",
+    "url": "/vinim93/trabajo-final.git/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "57f05cb8a664a2244e08",
+    "url": "/vinim93/trabajo-final.git/static/css/main.f4ddb60f.chunk.css"
+  },
+  {
+    "revision": "a33a30e03a728da90b3a",
+    "url": "/vinim93/trabajo-final.git/static/js/2.b1148353.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/vinim93/trabajo-final.git/static/js/2.b1148353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f05cb8a664a2244e08",
+    "url": "/vinim93/trabajo-final.git/static/js/main.8f361366.chunk.js"
+  },
+  {
+    "revision": "1ef71413c6a41c4fb946",
+    "url": "/vinim93/trabajo-final.git/static/js/runtime-main.f19866bb.js"
+  },
+  {
+    "revision": "c6f1884b215c9d6d6039f3efdd7e9be1",
+    "url": "/vinim93/trabajo-final.git/static/media/among.c6f1884b.webp"
+  },
+  {
+    "revision": "a026b634541db00ba6a2118b9f464c63",
+    "url": "/vinim93/trabajo-final.git/static/media/anime.a026b634.webp"
+  },
+  {
+    "revision": "4132117b509afcf51b2dd3bd02d87607",
+    "url": "/vinim93/trabajo-final.git/static/media/aven.4132117b.webp"
+  },
+  {
+    "revision": "787d44e7c2f37fd9f0b8099e729f91c3",
+    "url": "/vinim93/trabajo-final.git/static/media/avengers.787d44e7.png"
+  },
+  {
+    "revision": "96d311cd82a596a5665bd22ad395a9cc",
+    "url": "/vinim93/trabajo-final.git/static/media/facebook.96d311cd.webp"
+  },
+  {
+    "revision": "c8bcf78517b08b256edc347f3b85ff66",
+    "url": "/vinim93/trabajo-final.git/static/media/fondo.c8bcf785.jpg"
+  },
+  {
+    "revision": "57cc2e7d367f283f153884bbf96888ff",
+    "url": "/vinim93/trabajo-final.git/static/media/it1.57cc2e7d.webp"
+  },
+  {
+    "revision": "7f40a6f64eb860a083994e9b685dae2f",
+    "url": "/vinim93/trabajo-final.git/static/media/ready.7f40a6f6.webp"
+  },
+  {
+    "revision": "5f2f0ef22ab6b213fd6027563877499c",
+    "url": "/vinim93/trabajo-final.git/static/media/witcher.5f2f0ef2.webp"
+  }
+]);
